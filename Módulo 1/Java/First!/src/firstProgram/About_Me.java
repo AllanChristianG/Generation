58b66be2f25499360b740cdb0405bf41e4d073a0
Dev4Future <65303904+AllanChristianG@users.noreply.github.com>
2020-11-12 23:59:40 -0300

    First project in Java language

---
+++ Módulo 1/Java/First!/src/firstProgram/About_Me.java
@@ -0,0 +1,12 @@
+package firstProgram;
+
+public class About_Me {
+	public static void main(String[] args) {
+		System.out.println("About me: "
+				+ "\nMy name is Allan but you can call me Chris. I like playing guitar, watching animes"
+				+ "\nand eating. I am always eager to learn and help. My favorite foods are Strognoff, "
+				+ "\npizza and Xloaf. I'm talktive, friendly, I like to interact with random people"
+				+ "\nfrom different cultures.");
+	}
+
+}
---
+++ Módulo 1/Java/First!/src/firstProgram/Skills_And_Mindsets.java
@@ -0,0 +1,12 @@
+package firstProgram;
+
+public class Skills_And_Mindsets {
+
+	public static void main(String[] args) {
+		System.out.println("Thats a too way off answer for me. What brought me here or kept me"
+				+ "\ngoing was resilience. A vision of the big picture, always planning looking"
+				+ "\nforward long-terms goals");
+
+	}
+
+}
