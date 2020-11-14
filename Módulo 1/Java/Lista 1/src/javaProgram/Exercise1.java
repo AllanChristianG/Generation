e80452d81eb482347edad5cb0e666d0652acc926
Dev4Future <65303904+AllanChristianG@users.noreply.github.com>
2020-11-13 23:17:12 -0300

    Exercises using math expressions

---
+++ Módulo 1/Java/Lista 1/src/javaProgram/Exercise1.java
@@ -0,0 +1,20 @@
+package javaProgram;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		System.out.println("Insert your grades in sequence: ");
+		int grade1, grade2, grade3;
+		double average;
+		
+		Scanner read = new Scanner(System.in);
+		grade1 = read.nextInt();
+		grade2 = read.nextInt();
+		grade3 = read.nextInt();
+		
+		average = grade1 * 0.2 + grade2 * 0.3 + grade3 * .5;
+		System.out.print(average);
+	}
+}
