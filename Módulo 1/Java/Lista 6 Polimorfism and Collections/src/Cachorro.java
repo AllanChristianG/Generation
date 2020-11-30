a89f38b2750db2edd20d56a14cebf5e0a49f45bb
Dev4Future <65303904+AllanChristianG@users.noreply.github.com>
2020-11-30 08:47:00 -0300

    Ended list

---
+++ Módulo 1/Java/Lista 6 Polimorfism and Collections/src/Cachorro.java
@@ -0,0 +1,19 @@
+
+public class Cachorro extends Animal {
+
+	public Cachorro(String nome, int idade) {
+		super(nome, idade);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String emitirSom() {
+		
+		return "Latir";
+	}
+
+	public void qualAcao() {
+
+		System.out.println("Correr");
+	}
+
+}
---
+++ Módulo 1/Java/Lista 6 Polimorfism and Collections/src/Preguiça.java
@@ -0,0 +1,16 @@
+public class Preguica extends Animal {
+
+
+	public Preguica(String nome, int idade) {
+		super(nome, idade);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String emitirSom() {
+		return "Ahhhhhh Ahhhhhh";
+	}
+
+	public void qualAcao() {
+		System.out.println("Preguiça sobe em árvores");
+	}
+}
