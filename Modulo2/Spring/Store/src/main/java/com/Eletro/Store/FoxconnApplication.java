3ed8d9f09b30a88fbcd6ed43de242b5be40fa824
Dev4Future <65303904+AllanChristianG@users.noreply.github.com>
2020-12-17 19:21:58 -0300

    Foxconn eletro

---
+++ Modulo2/Spring/Store/src/main/java/com/Eletro/Store/FoxconnApplication.java
@@ -0,0 +1,13 @@
+package com.Eletro.Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxconnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxconnApplication.class, args);
+	}
+
+}
---
+++ Modulo2/Spring/Store/src/test/java/com/Eletro/Store/FoxconnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eletro.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxconnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
